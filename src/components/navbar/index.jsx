aa95d9ae27846cedd68b21c33f1e5b3cc6ecd558
bappyasif <bappyasif@hotmail.com>
2023-04-20 23:09:39 +0600

    added feature, adjusted ui
    * added a site log in header
    * adjusted navs and corresponding compoenents ui

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,19 +2,24 @@ import { navs } from '@/data'
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
-// import headerImg from "../../../public/newspapersPile.jpg"
 import headerImg from "../../../public/abstract.jpg"
+import logoImg from "../../../public/logo.png"
 import { useRouter } from 'next/router';
-// import headerImg from "../../../public/teamWork.jpg"
 
 export const AppNavigations = () => {
     const router = useRouter();
     console.log(router.pathname)
     return (
-        <header className='mb-8'>
-            {/* <HeaderImage /> */}
-            {/* <ReUsableImageComponent height={"130px"} width={"100%"} altText={"abstract image as a header background"} imgSrc={headerImg} /> */}
-            <ReUsableImageComponent height={"60px"} width={"100%"} altText={"abstract image as a header background"} imgSrc={headerImg} />
+        <header className='mb-12'>
+            <ReUsableImageComponent height={"101px"} width={"100%"} altText={"abstract image as a header background"} imgSrc={headerImg} />
+            <ReUsableImageComponent
+                height={"101px"}
+                width={"168px"}
+                // altText={"team work picture from unsplash used here as a background"}
+                // altText={"For Logo"}
+                altText={"For Fill"}
+                imgSrc={logoImg}
+            />
             <RenderNavs />
         </header>
     )
@@ -31,7 +36,7 @@ export const ReUsableImageComponent = ({ height, width, imgSrc, altText }) => {
                 zIndex: - 1
             }}
         >
-            <ImageComponent 
+            <ImageComponent
                 imgSrc={imgSrc}
                 altText={altText}
             />
@@ -39,56 +44,28 @@ export const ReUsableImageComponent = ({ height, width, imgSrc, altText }) => {
     )
 }
 
-export const ImageComponent = ({imgSrc, altText}) => {
+export const ImageComponent = ({ imgSrc, altText }) => {
     return (
         <Image
-                // className='fixed'
-                src={imgSrc}
-                alt={altText}
-                placeholder="blur"
-                quality={100}
-                style={{
-                    // rotate: "-360deg"
-                    // objectFit: "cover"
-                    objectFit: altText === "For Fill" ? "fill"  : "cover",
-                    height: altText === "For Fill" && "inherit",
-                    opacity: altText === "For Fill" && ".81"
-                }}
-            />
+            // className='fixed'
+            src={imgSrc}
+            alt={altText}
+            placeholder="blur"
+            quality={100}
+            style={{
+                objectFit: altText === "For Fill" ? "fill" : altText === "For Logo" ? "contain" : "cover",
+                height: altText === "For Fill" && "inherit",
+                opacity: altText === "For Fill" && ".81"
+            }}
+        />
     )
 }
-
-// const HeaderImage = () => {
-//     return (
-//         <div
-//             className='fixed'
-//             style={{
-//                 height: "130px",
-//                 width: "100%",
-//                 overflow: "hidden",
-//                 zIndex: - 1
-//             }}
-//         >
-//             <Image
-//                 // className='fixed'
-//                 src={headerImg}
-//                 alt='cityscape night view for site background'
-//                 placeholder="blur"
-//                 quality={100}
-//                 style={{
-//                     // rotate: "-360deg"
-//                     objectFit: "cover"
-//                 }}
-//             />
-//         </div>
-//     )
-// }
 
 const RenderNavs = () => {
     const renderNavs = () => navs.map(item => <RenderNav key={item.name} item={item} />);
 
     return (
-        <nav className='flex justify-end gap-2 w-full'>
+        <nav className='flex justify-end gap-4 w-full'>
             {renderNavs()}
         </nav>
     )
@@ -99,7 +76,7 @@ const RenderNav = ({ item }) => {
 
     return (
         <Link
-            className='flex items-center gap-2 px-2 text-2xl bg-zinc-600 text-zinc-200 rounded-md'
+            className='flex items-center gap-2 p-4 text-2xl bg-zinc-600 text-zinc-200 rounded-md'
             href={path}
         >
             <span>{name}</span>
